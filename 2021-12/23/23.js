3b218edd4b1f52bcdf79b5e026fe89c42c551c94
jihyun-jeon <jhplus13@naver.com>
2021-12-24 20:58:48 +0900

    2021.12.24 - 문자열에서 특정 문자 갯수 세는 함수 공부

--- 2021-12/23/23.js
+++ 2021-12/23/23.js
@@ -34,23 +34,43 @@ myFunction1([7, '+3', '*5', 4, '-12']); // 42
 
 /*
 // 문제2. 알파벳으로 구성된 문자열 중 연속된 문자의 갯수가 젤 많은 것의 숫자를 출력하는 함수를 만드시오. //while, indexOf 활용
+// 방법1 - 객체 활용 , for문 돌면서 obj에 값을 +1해나가는 방식으로
+// function myFunction2(str) {
+//   const arr = str.split('').sort(); // [a,a,a,b,c,d,d];
+//   const obj = {};
+
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i += 1) {
+//     if (arr[i] in obj) {
+//       count += 1;
+//     } else {
+//       count = 1;
+//     }
+
+//     obj[arr[i]] = count;
+//   }
+
+//   const result = Object.values(obj);
+//   console.log(Math.max(...result));
+// }
+
+// 방법2 - reduce 활용
 function myFunction2(str) {
-  const obj = {};
-  let count = 1;
-  for (let i = 0; i < str.length; i += 1) {
-    if (str[i] in obj) {
-      count += 1;
-    } else {
-      count = 1;
-    }
-    obj[str[i]] = count;
-  }
-  const result = Math.max(...Object.values(obj));
-  console.log(result);
+  return Math.max(
+    ...Object.values(
+      str.split('').reduce((obj, ch) => {
+        if (ch in obj) {
+          obj[ch] += 1;
+        } else {
+          obj[ch] = 1;
+        }
+
+        return obj;
+      }, {}), // 초기값 {}
+    ),
+  );
 }
 
-myFunction2('aaabcdd'); // 3
+myFunction2('dabacad'); // 3 //aaa b c dd
 myFunction2('abccccccdefff'); // 6
-
-// for문 돌면서 하는 법으로 풀어보기
 */
